a932995ce747d139b2ff4d94240de35a5ab53c78
subrata410 <subrata.rooy2022@gmail.com>
2023-01-23 20:32:18 +0600

    JavaScript-Methods

---
+++ script.js
@@ -0,0 +1,41 @@
+// (1) Arry Filter()
+// const age1 = [32, 33, 16, 17, 18, 6, 40];
+
+// function isVoter(vAge) {
+//     return vAge >= 18;
+// }
+
+// const voterList = age1.filter(isVoter);
+// console.log(voterList);
+
+// (2) Arry find()
+// const age1 = [32, 33, 35, 16, 17, 18, 6, 40, 50];
+
+// function checkPerson(pAge) {
+//     return pAge > 33;
+// }
+
+// const person = age1.find(checkPerson);
+// console.log(person);
+
+// (3) Arry map()
+// const num = [32, 33, 35, 16, 17, 18, 6, 40, 50];
+
+// function newFunction(n) {
+//     return n * 2;
+// }
+// const newNumber = num.map(newFunction);
+// console.log(newNumber);
+
+// (4) Arry reduce()
+// const num = [32, 33, 35, 16, 17, 18, 6, 40, 50];
+// function getTotal(first, next) {
+//     return first + next;
+// }
+// const total = num.reduce(getTotal);
+// console.log(total);
+
+// (4) Arry sort()
+// const fruits = ['Banana', 'Orange', 'Apple', 'Mango'];
+// fruits.sort();
+// console.log(fruits);
